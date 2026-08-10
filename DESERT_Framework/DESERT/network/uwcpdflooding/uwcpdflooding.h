780fad7226eecb3c325927a0155a9072d89f83b7
mike <kuzmish02@gmail.com>
2026-08-10 15:43:21 +0200

    CPDFLOODING 0.0.1

--- DESERT_Framework/DESERT/network/uwcpdflooding/uwcpdflooding.h
+++ DESERT_Framework/DESERT/network/uwcpdflooding/uwcpdflooding.h
@@ -152,6 +152,7 @@ private:
                                   in the disk. */
     std::ostringstream osstream_; /**< Used to convert to string. */
 
+	double t_delay_;
     double n_dupl_; /**< Number of duplicates threshold. */
 	double t_dupl_; /**< Time window for duplicates */
 	double te_;  /**< Transmission Efficiency */
